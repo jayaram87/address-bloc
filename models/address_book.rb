ea60d8fd20e3af9cbb72b1391a593b58be3d14a0
jayaram87 <jayaramraja1987@gmail.com>
2016-02-20 08:25:42 +0000

    completed assignment 22

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,3 +1,5 @@
+require "csv"
+
 class AddressBook
     attr_accessor :entries
     
@@ -5,6 +7,7 @@ class AddressBook
         @entries = []
     end
     
+    # adding new entries to the Address Book
     def add_entry(name, phone_number, email)
         index = 0
         @entries.each do |entry|
@@ -14,6 +17,56 @@ class AddressBook
             index += 1
         end
         @entries.insert(index, Entry.new(name, phone_number, email))
+    end
+    
+    # importing a csv file with names, phone numbers & emails
+    def import_from_csv(file_name)
+        csv_text = File.read(file_name)
+        csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+        csv.each do |row|
+            row_hash = row.to_hash
+            add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+        end
+    end
+    
+    # Searching the address book for a specific entry by name using the binary search
+    def binary_search(name)
+        # divide and conquer design pattern, searching using the array indexes
+        lower = 0
+        upper = @entries.length - 1
+        
+        while lower <= upper
+            mid = (lower+upper)/2
+            mid_name = @entries[mid].name
+            
+            if name == mid_name
+                return @entries[mid]
+            # how does compare name is less than mid_name
+            elsif name < mid_name
+                upper = mid - 1
+            elsif name > mid_name
+                lower = mid + 1
+            end
+        end
+        
+        return nil
+        
+    end
+    
+    def iterative_search(name)
+        index = 0
+        last_index = @entries.length - 1
+        
+        while index <= last_index
+            search_name = @entries[index].name
+            if name == search_name
+                return @entries[index].name
+            else
+                index += 1
+            end
+        end
+        
+        return nil
     end
 
 end
